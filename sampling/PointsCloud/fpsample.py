f85f404f6738e75a84737d89e89cea1ce377a948
WaleedAlzamil80 <WaleedAlzamil80@gmail.com>
2025-04-13 13:20:39 +0200

    smaller backet size

--- sampling/PointsCloud/fpsample.py
+++ sampling/PointsCloud/fpsample.py
@@ -17,5 +17,5 @@ def fpsample_fps(points, num_centroids=2048, k=32):
     return k_nearest_neighbors_optimized(points, centroids, k)
  
 def fps(points, num_centroids=2048):
-    kdline_fps_samples_idx = fpsample.bucket_fps_kdline_sampling(points, num_centroids, h=9)
+    kdline_fps_samples_idx = fpsample.bucket_fps_kdline_sampling(points, num_centroids, h=7)
     return points[kdline_fps_samples_idx], kdline_fps_samples_idx
